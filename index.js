4dc11b9377f86cf9a4dfc06c2c49a0569f4bb112
Matias Arias <matiasarias3103@gmail.comm>
2023-12-07 19:23:07 -0300

    [FEAT/FIX] Agregado del localStorage

--- index.js
+++ index.js
@@ -1,73 +1,105 @@
 /* Import html */
 const numeroGanadas = document.querySelector("#numeroVecesGanadas");
 const numeroPerdidas = document.querySelector("#numeroVecesPerdidas");
-const tituloVictoriasDerrotas = document.querySelector("#titulo");
-const botonMaquina = document.querySelector("#maquinaEleccion");
-const botonesUsuario = document.querySelectorAll(".user button");
-const arr = [];
-let v = 1;
-let d = 1;
+const tituloEleccion = document.querySelector("#titulo");
+const btnUsuario = document.querySelectorAll("#usuariobtn");
+const btnReset = document.querySelector("#btn-reset");
+const btnMaquina = document.querySelector("#maquinaEleccion");
+const victoriasPerdidas = {
+  victorias: 0,
+  perdida: 0,
+};
 
-/* Funcion de comparacion de maquina */
-function comparacionMaquina(maquina) {
-  if (maquina === 1) {
-    botonMaquina.innerHTML = "Piedra";
+/* Carga de la pagina */
+window.addEventListener("load", () => {
+  numeroGanadas.innerHTML = localStorage.getItem("Ganadas") || 0;
+  numeroPerdidas.innerHTML = localStorage.getItem("Perdidas") || 0;
+});
+
+/* Click del usuario */
+btnUsuario.forEach((element) => {
+  element.addEventListener("click", ({ target: { value } } = e) => {
+    ganadas(value, maquina(numeroAlazar()));
+    perdida(value, maquina(numeroAlazar()));
+    empate(value, maquina(numeroAlazar()));
+  });
+});
+
+/* Funcion numero al azar */
+const numeroAlazar = () => Math.floor(Math.random() * 3 + 1);
+
+/* Funcion maquina eleccion */
+const maquina = (numeroAzar) => {
+  if (numeroAzar === 1) {
+    btnMaquina.innerHTML = "🪨";
     return "Piedra";
-  } else if (maquina === 2) {
-    botonMaquina.innerHTML = "Papel";
+  }
+  if (numeroAzar === 2) {
+    btnMaquina.innerHTML = "🧻";
     return "Papel";
-  } else if (maquina === 3) {
-    botonMaquina.innerHTML = "Tijera";
+  }
+  if (numeroAzar === 3) {
+    btnMaquina.innerHTML = "✂️";
     return "Tijera";
   }
-}
+};
 
-/* Compara y si maquina y usuario son iguales, llama a la funcion contador */
-function comparar(usuario, maquina) {
-  let bot = comparacionMaquina(maquina);
-  contador(usuario, bot);
-}
+/* Ganadas */
+const ganadas = (usuario, maquina) => {
+  usuario === "Piedra" && maquina === "Tijera" && functionTitulo(maquina);
+  usuario === "Papel" && maquina === "Piedra" && functionTitulo(maquina);
+  usuario === "Tijera" && maquina === "Papel" && functionTitulo(maquina);
+};
 
-/* Numero al azar para comparar maquina */
-function numeroAlAzar() {
-  let numero = Math.floor(Math.random() * 3 + 1);
-  return numero;
-}
+/* Titulo ganada */
+const functionTitulo = (maquina) => {
+  tituloEleccion.innerHTML = `Haz ganado la maquina eligio ${maquina}`;
+  victoriasPerdidas.victorias++;
+  tituloVictorias();
+};
 
-/* contador de victorias y derrotas */
-function contador(usuario, maquina) {
-  /* Empate */
-  if (usuario === maquina) {
-    tituloVictoriasDerrotas.innerHTML = `Empate! La maquina eligio ${maquina}`;
-  }
-  /* Victorias */
-  if (usuario === "Piedra" && maquina === "Tijera") {
-    numeroGanadas.innerHTML = v++;
-    tituloVictoriasDerrotas.innerHTML = `Ganaste! La maquina eligio ${maquina}`;
-  } else if (usuario === "Papel" && maquina === "Piedra") {
-    numeroGanadas.innerHTML = v++;
-    tituloVictoriasDerrotas.innerHTML = `Ganaste! La maquina eligio ${maquina}`;
-  } else if (usuario === "Tijera" && maquina === "Papel") {
-    numeroGanadas.innerHTML = v++;
-    tituloVictoriasDerrotas.innerHTML = `Ganaste! La maquina eligio ${maquina}`;
-  }
-  /* Derrotas */
-  if (usuario === "Piedra" && maquina === "Papel") {
-    numeroPerdidas.innerHTML = d++;
-    tituloVictoriasDerrotas.innerHTML = `Perdiste la maquina eligio ${maquina}`;
-  } else if (usuario === "Papel" && maquina === "Tijera") {
-    numeroPerdidas.innerHTML = d++;
-    tituloVictoriasDerrotas.innerHTML = `Perdiste la maquina eligio ${maquina}`;
-  } else if (usuario === "Tijera" && maquina === "Piedra") {
-    numeroPerdidas.innerHTML = d++;
-    tituloVictoriasDerrotas.innerHTML = `Perdiste la maquina eligio ${maquina}`;
-  }
-}
+/* Funcion contador de victorias */
+const tituloVictorias = () => {
+  numeroGanadas.innerHTML = victoriasPerdidas.victorias;
+  localStorage.setItem("Ganadas", JSON.stringify(victoriasPerdidas.victorias));
+};
 
-botonesUsuario.forEach((element) => {
-  element.addEventListener("click", () => {
-    console.log(element.textContent);
-    const eleccionUsuario = element.textContent;
-    comparar(eleccionUsuario, numeroAlAzar());
-  });
+/* Perdida */
+const perdida = (usuario, maquina) => {
+  usuario === "Piedra" && maquina === "Papel" && functionTituloPerdida(maquina);
+  usuario === "Papel" && maquina === "Tijera" && functionTituloPerdida(maquina);
+  usuario === "Tijera" &&
+    maquina === "Piedra" &&
+    functionTituloPerdida(maquina);
+};
+
+/* Titulo perdida */
+const functionTituloPerdida = (maquina) => {
+  tituloEleccion.innerHTML = `Haz perdido la maquina eligio ${maquina}`;
+  victoriasPerdidas.perdida++;
+  tituloPerdidas();
+};
+
+/* Contador de perdidas */
+const tituloPerdidas = () => {
+  numeroPerdidas.innerHTML = victoriasPerdidas.perdida;
+  localStorage.setItem("Perdidas", JSON.stringify(victoriasPerdidas.perdida));
+};
+
+/* Empate */
+const empate = (usuario, maquina) => {
+  usuario === maquina && functionTituloEmpate(maquina);
+};
+
+/* Funcion titulo empate */
+const functionTituloEmpate = (maquina) => {
+  tituloEleccion.innerHTML = `Empate la maquina eligio ${maquina}`;
+};
+
+/* Boton de reset */
+btnReset.addEventListener("click", () => {
+  localStorage.removeItem("Perdidas");
+  localStorage.removeItem("Ganadas");
+  numeroGanadas.innerHTML = 0;
+  numeroPerdidas.innerHTML = 0;
 });
